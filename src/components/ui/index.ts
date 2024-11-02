555ff475f365890798c53f620bf8f3aeef20983b
Muhammad Ghazanfar <129008293+MohdGhazanfar@users.noreply.github.com>
2024-11-02 15:56:42 +0500

    Delete src directory

--- src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Alert, AlertDescription } from './Alert'
-export { default as Button } from './Button'
-export { 
-  Select,
-  SelectTrigger,
-  SelectContent,
-  SelectItem,
-  useSelectContext 
-} from './Select'
